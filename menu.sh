8983616adc3230755800701542e4cf75cc48a9ae
Ayush <ayu.chandekar@gmail.com>
2024-03-13 20:36:03 +0530

    Add the feature for showing the key to add to github and setting the key for git
    Signed-off-by: Ayush <ayu.chandekar@gmail.com>

--- menu.sh
+++ menu.sh
@@ -9,6 +9,11 @@ display
   then
     printf '\n'
     gpg --full-generate-key
+    keyId=$(gpg --list-secret-keys --keyid-format=long | awk '$1 ~ /sec/ {print $2}' | awk -F "/" '{print $2}' | head -1)
+    git config --global user.signingkey $keyId
+    git config --global commit.gpgsign true
+    (gpg --armor --export $key) | lolcat
+    (printf '\n%s\n\n' "====Add the above key to github=====") | lolcat
     menu
   elif [ $input == 2 ]
   then 
